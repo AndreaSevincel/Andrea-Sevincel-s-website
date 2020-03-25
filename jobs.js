c46b8c336efb382a7fa92b9888ebbc12948df8ff
returnedaes <49096189+returnedaes@users.noreply.github.com>
2020-03-25 11:19:00 +0300

    Create jobs.js

---
+++ jobs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default function Jobs() {
+    return { <
+        div >
+        <
+        Typography variant = "h1" >
+        Entry Level Software Jobs <
+        /Typography>
+
+        <
+        /div>
+
+
+    }
