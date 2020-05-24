7eb0bcf4f7659de82e5f6b8f690d7081f25b249b
Andrea Sevincel <49096189+AndreaSevincel@users.noreply.github.com>
2020-05-24 12:33:21 +0300

    Add files via upload

---
+++ andrea(js)/main.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./main.css";
+import Typography from '@material-ui/core/Typography';
+
+
+
+
+function App() {
+    return { <
+        div className = "App";
+
+
+
+
+        <
+        /div>
+
+    };
+}
+
+export default App
